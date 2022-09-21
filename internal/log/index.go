72e0772050c8a321477d3ef0129e5043ff2ed499
Vadim Prokoshin <vpro85@gmail.com>
2022-09-21 17:56:30 +0200

    add Write()

--- internal/log/index.go
+++ internal/log/index.go
@@ -70,3 +70,13 @@ func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
 	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
 	return out, pos, nil
 }
+
+func (i *index) Write(off uint32, pos uint64) error {
+	if uint64(len(i.mmap)) < i.size+entWidth {
+		return io.EOF
+	}
+	enc.PutUint32(i.mmap[i.size:i.size+offWidth], off)
+	enc.PutUint64(i.mmap[i.size+offWidth:i.size+entWidth], pos)
+	i.size += uint64(entWidth)
+	return nil
+}
