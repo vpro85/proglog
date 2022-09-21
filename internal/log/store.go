5ca86eff1c0efaca3b08b9bcdc4ba94d216bbec1
Vadim Prokoshin <vpro85@gmail.com>
2022-09-21 14:55:15 +0200

    add Append() to store

--- internal/log/store.go
+++ internal/log/store.go
@@ -34,3 +34,19 @@ func newStore(f *os.File) (*store, error) {
 		buf:  bufio.NewWriter(f),
 	}, nil
 }
+
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
