157a0f8d0d436831e0b490b4304f70a249e328fa
Vadim Prokoshin <vpro85@gmail.com>
2022-09-21 18:03:31 +0200

    add TestIndex

---
+++ internal/log/index_test.go
@@ -0,0 +1,30 @@
+package log
+
+import (
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestIndex(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "index_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+	idx, err := newIndex(f, c)
+	require.NoError(t, err)
+
+	_, _, err = idx.Read(-1)
+	require.Error(t, err)
+	require.Equal(t, f.Name(), idx.Name())
+	entries := []struct {
+		Off uint32
+		Pos uint64
+	}{
+		{Off: 0, Pos: 0},
+		{Off: 1, Pos: 10},
+	}
+}
