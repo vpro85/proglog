26e273053b92feba2777a09b708f8af0d681244d
Vadim Prokoshin <vpro85@gmail.com>
2022-09-22 11:54:46 +0200

    add TestIndex

--- internal/log/index_test.go
+++ internal/log/index_test.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"github.com/stretchr/testify/require"
+	"io"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -27,4 +28,26 @@ func TestIndex(t *testing.T) {
 		{Off: 0, Pos: 0},
 		{Off: 1, Pos: 10},
 	}
+
+	for _, want := range entries {
+		err = idx.Write(want.Off, want.Pos)
+		require.NoError(t, err)
+
+		_, pos, err := idx.Read(int64(want.Off))
+		require.NoError(t, err)
+		require.Equal(t, want.Pos, pos)
+	}
+	// index and scanner should error when reading past existing entries
+	_, _, err = idx.Read(int64(len(entries)))
+	require.Equal(t, io.EOF, err)
+	_ = idx.Close()
+
+	// index should build its state from the existing file
+	f, _ = os.OpenFile(f.Name(), os.O_RDWR, 0600)
+	idx, err = newIndex(f, c)
+	require.NoError(t, err)
+	off, pos, err := idx.Read(-1)
+	require.NoError(t, err)
+	require.Equal(t, uint32(1), off)
+	require.Equal(t, entries[1], pos)
 }
