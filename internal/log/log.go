963501970ed0f344aebdb276456ed2985c683e59
Vadim Prokoshin <vpro85@gmail.com>
2022-09-22 15:25:44 +0200

    Add Read() and originReader struct

--- internal/log/log.go
+++ internal/log/log.go
@@ -162,3 +162,14 @@ func (l *Log) Reader() io.Reader {
 	}
 	return io.MultiReader(readers...)
 }
+
+type originReader struct {
+	*store
+	off int64
+}
+
+func (o *originReader) Read(p []byte) (int, error) {
+	n, err := o.ReadAt(p, o.off)
+	o.off += int64(n)
+	return n, err
+}
