db35c5ef08e74d5eaada7c4de98fd1ac974a2f7e
Vadim Prokoshin <vpro85@gmail.com>
2022-09-22 16:33:48 +0200

    add testAppendRead, testOutOfRangeErr

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"os"
+	api "proglog/api/v1"
 	"testing"
 )
 
@@ -11,9 +12,9 @@ func TestLog(t *testing.T) {
 	for scenario, fn := range map[string]func(t *testing.T, log *Log){
 		"append and read a record succeeds": testAppendRead,
 		"offset out of range error":         testOutOfRangeErr,
-		"init with existing segments":       testInitExisting,
-		"reader":                            testReader,
-		"truncate":                          testTruncate,
+		//"init with existing segments":       testInitExisting,
+		//"reader":                            testReader,
+		//"truncate":                          testTruncate,
 	} {
 		t.Run(scenario, func(t *testing.T) {
 			dir, err := ioutil.TempDir("", "store-test")
@@ -26,4 +27,23 @@ func TestLog(t *testing.T) {
 			fn(t, log)
 		})
 	}
+}
+
+func testAppendRead(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	off, err := log.Append(append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, append.Value, read.Value)
+}
+
+func testOutOfRangeErr(t *testing.T, log *Log) {
+	read, err := log.Read(1)
+	require.Nil(t, read)
+	require.Error(t, err)
 }
