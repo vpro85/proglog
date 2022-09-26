f222c2e456a98085650b5ee7b85d338a870d5ad4
Vadim Prokoshin <vpro85@gmail.com>
2022-09-26 18:03:59 +0200

    add testConsumePastBoundary

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -18,8 +18,8 @@ func TestServer(t *testing.T) {
 		config *Config,
 	){
 		"produce/consume a message to/from the log succeeds": testProduceConsume,
-		//"produce/consume stream succeeds":                    testProduceConsumeStream,
-		//"consume past log boundary fails":                    testConsumePastBoundary,
+		"produce/consume stream succeeds":                    testProduceConsumeStream,
+		"consume past log boundary fails":                    testConsumePastBoundary,
 	} {
 		t.Run(scenario, func(t *testing.T) {
 			client, config, teardown := setupTest(t, nil)
@@ -80,4 +80,24 @@ func testProduceConsume(t *testing.T, client api.LogClient, config *Config) {
 	require.NoError(t, err)
 	require.Equal(t, want.Value, consume.Record.Value)
 	require.Equal(t, want.Offset, consume.Record.Offset)
+}
+
+func testConsumePastBoundary(t *testing.T, client api.LogClient, config *Config) {
+	ctx := context.Background()
+	produce, err := client.Produce(ctx, &api.ProduceRequest{Record: &api.Record{Value: []byte("hello world")}})
+	require.NoError(t, err)
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{Offset: produce.Offset + 1})
+	if consume != nil {
+		t.Fatal("consume not nil")
+	}
+	got := grpc.Code(err)
+	want := grpc.Code(api.ErrOffsetOutOfRange{}.GRPCStatus().Err())
+	if got != want {
+		t.Fatalf("got err: %v, want: %v", got, want)
+	}
+}
+
+func testProduceConsumeStream(t *testing.T, client api.LogClient, config *Config) {
+
 }
