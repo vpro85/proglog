43be7c7c824967b9501fe87237910b71ec936388
Vadim Prokoshin <vpro85@gmail.com>
2022-09-26 17:29:09 +0200

    add TestServer

---
+++ internal/server/server_test.go
@@ -0,0 +1,70 @@
+package server
+
+import (
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"io/ioutil"
+	"net"
+	api "proglog/api/v1"
+	"proglog/internal/log"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		client api.LogClient,
+		config *Config,
+	){
+		//"produce/consume a message to/from the log succeeds": testProduceConsume,
+		//"produce/consume stream succeeds":                    testProduceConsumeStream,
+		//"consume past log boundary fails":                    testConsumePastBoundary,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			client, config, teardown := setupTest(t, nil)
+			defer teardown()
+			fn(t, client, config)
+		})
+	}
+}
+
+func setupTest(t *testing.T, fn func(config *Config)) (
+	client api.LogClient,
+	cfg *Config,
+	teardown func(),
+) {
+	t.Helper()
+	l, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	clientOptions := []grpc.DialOption{grpc.WithInsecure()}
+	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
+	require.NoError(t, err)
+
+	dir, err := ioutil.TempDir("", "server-test")
+	require.NoError(t, err)
+
+	clog, err := log.NewLog(dir, log.Config{})
+	require.NoError(t, err)
+
+	cfg = &Config{
+		CommitLog: clog,
+	}
+	if fn != nil {
+		fn(cfg)
+	}
+	server, err := NewGRPCServer(cfg)
+	require.NoError(t, err)
+
+	go func() {
+		server.Serve(l)
+	}()
+	client = api.NewLogClient(сс)
+
+	return client, cfg, func() {
+		server.Stop()
+		cc.Close()
+		l.Close()
+		clog.Remove()
+	}
+}
