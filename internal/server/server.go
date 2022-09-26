35a9d0d3930f60ac9506cbfd23ec8ce2184bc5c4
Vadim Prokoshin <vpro85@gmail.com>
2022-09-26 15:30:18 +0200

    add grpc server

---
+++ internal/server/server.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	api "proglog/api/v1"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
