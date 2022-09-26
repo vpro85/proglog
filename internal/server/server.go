6e78305b58ae17e0873b84ba3b622563e30da242
Vadim Prokoshin <vpro85@gmail.com>
2022-09-26 15:38:09 +0200

    add Consume() to grpc server

--- internal/server/server.go
+++ internal/server/server.go
@@ -30,3 +30,11 @@ func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api
 	}
 	return &api.ProduceResponse{Offset: offset}, nil
 }
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
