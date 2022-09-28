33a8a79f4e49c6b11739354955a23ce97af9f160
Vadim Prokoshin <vpro85@gmail.com>
2022-09-28 19:35:09 +0200

    WIP: add telemetryExporter to test

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"github.com/stretchr/testify/require"
+	"go.opencensus.io/examples/exporter"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -17,6 +18,7 @@ import (
 	"proglog/internal/config"
 	"proglog/internal/log"
 	"testing"
+	"time"
 )
 
 var debug = flag.Bool("debug", false, "Enable observability for debugging.")
@@ -116,6 +118,26 @@ func setupTest(t *testing.T, fn func(config *Config)) (
 
 	authorizer := auth.New(config.ACLModelFile, config.ACLPolicyFile)
 
+	var telemetryExporter *exporter.LogExporter
+	if *debug {
+		metricsLogFile, err := ioutil.TempFile("", "metrics-*.log")
+		require.NoError(t, err)
+		t.Logf("metrics log file: %s", metricsLogFile.Name())
+
+		tracesLogFile, err := ioutil.TempFile("", "traces-*.log")
+		require.NoError(t, err)
+		t.Logf("traces log file: %s", tracesLogFile.Name())
+
+		telemetryExporter, err = exporter.NewLogExporter(exporter.Options{
+			MetricsLogFile:    metricsLogFile.Name(),
+			TracesLogFile:     tracesLogFile.Name(),
+			ReportingInterval: time.Second,
+		})
+		require.NoError(t, err)
+		err = telemetryExporter.Start()
+		require.NoError(t, err)
+	}
+
 	cfg = &Config{
 		CommitLog:  clog,
 		Authorizer: authorizer,
@@ -135,6 +157,11 @@ func setupTest(t *testing.T, fn func(config *Config)) (
 		rootConn.Close()
 		nobodyConn.Close()
 		l.Close()
+		if telemetryExporter != nil {
+			time.Sleep(1500 * time.Millisecond)
+			telemetryExporter.Stop()
+			telemetryExporter.Close()
+		}
 	}
 }
 
