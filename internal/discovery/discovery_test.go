56d3b53afc9b37a442d583904a1b216e38a175b5
Vadim Prokoshin <vpro85@gmail.com>
2022-09-29 12:37:11 +0200

    Add test for service discovery

---
+++ internal/discovery/discovery_test.go
@@ -0,0 +1,31 @@
+package discovery
+
+import (
+	"fmt"
+	"github.com/hashicorp/serf/serf"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestMembership(t *testing.T) {
+	m, handler := setupMember(t, nil)
+	m, _ = setupMember(t, m)
+	m, _ = setupMember(t, m)
+	require.Eventually(t, func() bool {
+		return 2 == len(handler.joins) &&
+			3 == len(m[0].Members()) &&
+			0 == len(handler.leaves)
+	}, 3*time.Second, 250*time.Millisecond)
+
+	require.NoError(t, m[2].Leave())
+
+	require.Eventually(t, func() bool {
+		return 2 == len(handler.joins) &&
+			3 == len(m[0].Members()) &&
+			serf.StatusLeft == m[0].Members()[2].Status &&
+			1 == len(handler.leaves)
+	}, 3*time.Second, 250*time.Millisecond)
+
+	require.Equal(t, fmt.Sprintf("%d", 2), <-handler.leaves)
+}
