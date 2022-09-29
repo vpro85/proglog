9cf7ad1cf87baadaec6637f9d158c7da06caaf1e
Vadim Prokoshin <vpro85@gmail.com>
2022-09-29 12:48:36 +0200

    Add setupMembers for test

--- internal/discovery/discovery_test.go
+++ internal/discovery/discovery_test.go
@@ -29,3 +29,30 @@ func TestMembership(t *testing.T) {
 
 	require.Equal(t, fmt.Sprintf("%d", 2), <-handler.leaves)
 }
+
+func setupMember(t *testing.T, members []*Membership) ([]*Membership, *handler) {
+	id := len(members)
+	ports := dynaport.Get(1)
+	addr := fmt.Sprintf("%s:%d", "127.0.0.1", ports[0])
+	tags := map[string]string{
+		"rpc_addr": addr,
+	}
+	c := Config{
+		NodeName: fmt.Sprintf("%d", id),
+		BindAddr: addr,
+		Tags:     tags,
+	}
+	h := &handler{}
+	if len(members) == 0 {
+		h.joins = make(chan map[string]string, 3)
+		h.leaves = make(chan string, 3)
+	} else {
+		c.StartJoinAddrs = []string{
+			members[0].BindAddr,
+		}
+	}
+	m, err := New(h, c)
+	require.NoError(t, err)
+	members = append(members, m)
+	return members, h
+}
