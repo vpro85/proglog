c47e87c7e131ef4dd716f54c09ce5f4776fcb7cc
Vadim Prokoshin <vpro85@gmail.com>
2022-09-29 16:33:47 +0200

    WIP: Add TestAgent

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -1,9 +1,15 @@
 package agent_test
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+	"io/ioutil"
+	"os"
+	"proglog/internal/agent"
 	"proglog/internal/config"
 	"testing"
+	"time"
 )
 
 func TestAgent(t *testing.T) {
@@ -23,4 +29,40 @@ func TestAgent(t *testing.T) {
 		ServerAddress: "127.0.0.1",
 	})
 	require.NoError(t, err)
+
+	var agents []*agent.Agent
+	for i := 0; i < 3; i++ {
+		ports := dynaport.Get(2)
+		bindAddr := fmt.Sprintf("%s:%d", "127.0.0.1", ports[0])
+		rpcPort := ports[1]
+
+		dataDir, err := ioutil.TempDir("", "agent-test-log")
+		require.NoError(t, err)
+
+		var startJoinAddrs []string
+		if i != 0 {
+			startJoinAddrs = append(startJoinAddrs, agents[0].Config.BindAddr)
+		}
+		agent, err := agent.New(agent.Config{
+			ServerTLSConfig: serverTLSConfig,
+			PeerTLSConfig:   peerTLSConfig,
+			DataDir:         dataDir,
+			BindAddr:        bindAddr,
+			RPCPort:         rpcPort,
+			NodeName:        fmt.Sprintf("%d", i),
+			StartJoinAddrs:  startJoinAddrs,
+			ACLModeFile:     config.ACLModelFile,
+			ACLPolicyFile:   config.ACLPolicyFile,
+		})
+		require.NoError(t, err)
+		agents = append(agents, agent)
+	}
+	defer func() {
+		for _, agent := range agents {
+			err := agent.Shutdown()
+			require.NoError(t, err)
+			require.NoError(t, os.RemoveAll(agent.Config.DataDir))
+		}
+	}()
+	time.Sleep(3 * time.Second)
 }
