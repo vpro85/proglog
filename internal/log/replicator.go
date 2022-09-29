00dacad766444c711605043ccbd9a99cc5237ced
Vadim Prokoshin <vpro85@gmail.com>
2022-09-29 14:03:29 +0200

    Add Join() to Replicator

--- internal/log/replicator.go
+++ internal/log/replicator.go
@@ -17,3 +17,20 @@ type Replicator struct {
 	closed  bool
 	close   chan struct{}
 }
+
+func (r *Replicator) Join(name, addr string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.init()
+	if r.closed {
+		return nil
+	}
+	if _, ok := r.servers[name]; ok {
+		// already replicated so skip
+		return nil
+	}
+	r.servers[name] = make(chan struct{})
+
+	go r.replicate(addr, r.servers[name])
+	return nil
+}
