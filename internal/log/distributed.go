1d56c67d3f11e9de210f10b35ecd10b8bdaa4349
Vadim Prokoshin <vpro85@gmail.com>
2022-09-30 15:22:33 +0200

    Add funcs to logStore

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -255,6 +255,49 @@ type logStore struct {
 	*Log
 }
 
+func (l *logStore) FirstIndex() (uint64, error) {
+	return l.LowesOffset()
+}
+
+func (l *logStore) LastIndex() (uint64, error) {
+	off, err := l.HighestOffset()
+	return off, err
+}
+
+func (l *logStore) GetLog(index uint64, out *raft.Log) error {
+	in, err := l.Read(index)
+	if err != nil {
+		return err
+	}
+	out.Data = in.Value
+	out.Index = in.Offset
+	out.Type = raft.LogType(in.Type)
+	out.Term = in.Term
+	return nil
+}
+
+func (l *logStore) StoreLog(record *raft.Log) error {
+	return l.StoreLogs([]*raft.Log{record})
+}
+
+func (l *logStore) StoreLogs(records []*raft.Log) error {
+	for _, record := range records {
+		if _, err := l.Append(&api.Record{
+			Value: record.Data,
+			Term:  record.Term,
+			Type:  uint32(record.Type),
+		}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *logStore) DeleteRange(min, max uint64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
 func newLogStore(dir string, c Config) (*logStore, error) {
 	log, err := NewLog(dir, c)
 	if err != nil {
