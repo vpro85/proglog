8385d1c7c88742a21c2979b4049707cc041e4a3d
Vadim Prokoshin <vpro85@gmail.com>
2022-09-30 12:06:08 +0200

    Add setupLog() to DistributedLog

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -1,6 +1,10 @@
 package log
 
-import "github.com/hashicorp/raft"
+import (
+	"github.com/hashicorp/raft"
+	"os"
+	"path/filepath"
+)
 
 type DistributedLog struct {
 	config Config
@@ -19,4 +23,14 @@ func NewDistributedLog(dataDir string, config Config) (*DistributedLog, error) {
 		return nil, err
 	}
 	return l, nil
+}
+
+func (l *DistributedLog) setupLog(dataDir string) error {
+	logDir := filepath.Join(dataDir, "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+	var err error
+	l.log, err = NewLog(logDir, l.config)
+	return err
 }
