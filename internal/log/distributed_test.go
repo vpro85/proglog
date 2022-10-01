beef96085a265c525017b971044e6968559eaed3
Vadim Prokoshin <vpro85@gmail.com>
2022-10-01 11:27:42 +0200

    WIP: Add TestMultipleNodes

---
+++ internal/log/distributed_test.go
@@ -0,0 +1,37 @@
+package log_test
+
+import (
+	"fmt"
+	"github.com/hashicorp/raft"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+	"io/ioutil"
+	"net"
+	"os"
+	"proglog/internal/log"
+	"testing"
+	"time"
+)
+
+func TestMultipleNodes(t *testing.T) {
+	var logs []*log.DistributedLog
+	nodeCount := 3
+	ports := dynaport.Get(nodeCount)
+	for i := 0; i < nodeCount; i++ {
+		dataDir, err := ioutil.TempDir("", "distributed-log-test")
+		require.NoError(t, err)
+		defer func(dir string) {
+			_ = os.RemoveAll(dir)
+		}(dataDir)
+		ln, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", ports[i]))
+		require.NoError(t, err)
+
+		config := log.Config{}
+		config.Raft.StreamLayer = log.NewStreamLayer(ln, nil, nil)
+		config.Raft.LocalID = raft.ServerID(fmt.Sprintf("%d", i))
+		config.Raft.HeartbeatTimeout = 50 * time.Millisecond
+		config.Raft.ElectionTimeout = 50 * time.Millisecond
+		config.Raft.LeaderLeaseTimeout = 50 * time.Millisecond
+		config.Raft.CommitTimeout = 50 * time.Millisecond
+	}
+}
