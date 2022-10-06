506bcce45dfcf7697100344ef60716de30afc8f1
Vadim Prokoshin <vpro85@gmail.com>
2022-10-06 15:00:17 +0200

    Fix Agent Test

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -14,6 +14,7 @@ import (
 	api "proglog/api/v1"
 	"proglog/internal/agent"
 	"proglog/internal/config"
+	"proglog/internal/loadbalance"
 	"testing"
 	"time"
 )
@@ -83,6 +84,8 @@ func TestAgent(t *testing.T) {
 		},
 	)
 	require.NoError(t, err)
+	// wait until replication has finished
+	time.Sleep(3 * time.Second)
 	consumeResponse, err := leaderClient.Consume(
 		context.Background(),
 		&api.ConsumeRequest{
@@ -91,8 +94,6 @@ func TestAgent(t *testing.T) {
 	)
 	require.NoError(t, err)
 	require.Equal(t, consumeResponse.Record.Value, []byte("foo"))
-	// wait until replication has finished
-	time.Sleep(3 * time.Second)
 
 	followerClient := client(t, agents[1], peerTLSConfig)
 	consumeResponse, err = followerClient.Consume(
@@ -126,7 +127,7 @@ func client(
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
 	rpcAddr, err := agent.Config.RPCAddr()
 	require.NoError(t, err)
-	conn, err := grpc.Dial(fmt.Sprintf("%s", rpcAddr), opts...)
+	conn, err := grpc.Dial(fmt.Sprintf("%s:///%s", loadbalance.Name, rpcAddr), opts...)
 	require.NoError(t, err)
 	client := api.NewLogClient(conn)
 	return client
