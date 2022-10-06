768ff05af704fa410713fee6441c3c720f63fccc
Vadim Prokoshin <vpro85@gmail.com>
2022-10-06 16:03:29 +0200

    Add setupFlags()

--- cmd/proglog/main.go
+++ cmd/proglog/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"log"
+	"os"
+	"path"
+	"proglog/internal/agent"
+	"proglog/internal/config"
 )
 
 func main() {
@@ -18,4 +23,38 @@ func main() {
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type cli struct {
+	cfg cfg
+}
+
+type cfg struct {
+	agent.Config
+	ServerTLSConfig config.TLSConfig
+	PeerTLSConfig   config.TLSConfig
+}
+
+func setupFlags(cmd *cobra.Command) error {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmd.Flags().String("config-file", "", "Path to config file.")
+	dataDir := path.Join(os.TempDir(), "proglog")
+	cmd.Flags().String("data-dir", dataDir, "Directory to store log and Raft data.")
+	cmd.Flags().String("node-name", hostname, "Unique server ID.")
+	cmd.Flags().String("bind-addr", "127.0.0.1:8401", "Address to bind Serf on.")
+	cmd.Flags().Int("rpc-port", 8400, "Port for RPC clients (and Raft) connections.")
+	cmd.Flags().StringSlice("start-join-addrs", nil, "Serf addresses to join.")
+	cmd.Flags().Bool("bootstrap", false, "Bootstrap the cluster.")
+	cmd.Flags().String("acl-model-file", "", "Path to ACL model.")
+	cmd.Flags().String("acl-policy-file", "", "Path to ACL policy.")
+	cmd.Flags().String("server-tls-cert-file", "", "Path to server tls cert.")
+	cmd.Flags().String("server-tls-key-file", "", "Path to server tls key.")
+	cmd.Flags().String("server-tls-ca-file", "", "Path to server certificate authority.")
+	cmd.Flags().String("peer-tls-cert-file", "", "Path to peer tls cert.")
+	cmd.Flags().String("peer-tls-key-file", "", "Path to peer tls key.")
+	cmd.Flags().String("peer-tls-ca-file", "", "Path to peer certificate authority.")
+	return viper.BindPFlags(cmd.Flags())
 }
--- go.mod
+++ go.mod
@@ -6,17 +6,19 @@ require (
 	github.com/casbin/casbin v1.9.1
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/hashicorp/go.net v0.0.1 // indirect
 	github.com/hashicorp/memberlist v0.4.0 // indirect
 	github.com/hashicorp/raft v1.1.1 // indirect
 	github.com/hashicorp/raft-boltdb v0.0.0-20171010151810-6e5ba93211ea // indirect
-	github.com/hashicorp/serf v0.8.5
+	github.com/hashicorp/serf v0.9.7
 	github.com/soheilhy/cmux v0.1.5 // indirect
 	github.com/spf13/cobra v1.5.0 // indirect
-	github.com/stretchr/testify v1.7.0
+	github.com/spf13/viper v1.13.0 // indirect
+	github.com/stretchr/testify v1.8.0
 	github.com/travisjeffery/go-dynaport v1.0.0
 	github.com/tysonmote/gommap v0.0.2
 	go.opencensus.io v0.23.0
-	go.uber.org/zap v1.10.0
+	go.uber.org/zap v1.17.0
 	golang.org/x/net v0.0.0-20220927171203-f486391704dc // indirect
 	golang.org/x/sys v0.0.0-20220927170352-d9d178bc13c6 // indirect
 	google.golang.org/genproto v0.0.0-20220927151529-dcaddaf36704
