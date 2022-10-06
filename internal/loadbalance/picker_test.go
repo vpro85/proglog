73cfb65842b9a5c4841cf0d971c659fdf4577a58
Vadim Prokoshin <vpro85@gmail.com>
2022-10-06 14:32:05 +0200

    Add setupTest()

--- internal/loadbalance/picker_test.go
+++ internal/loadbalance/picker_test.go
@@ -2,7 +2,10 @@ package loadbalance_test
 
 import (
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/balancer/base"
+	"google.golang.org/grpc/resolver"
 	"proglog/internal/loadbalance"
 	"testing"
 )
@@ -20,4 +23,38 @@ func TestPickerNoSubConnAvailable(t *testing.T) {
 		require.Equal(t, balancer.ErrNoSubConnAvailable, err)
 		require.Nil(t, result.SubConn)
 	}
+}
+
+func TestPickerProducesToLeader(t *testing.T) {
+	picker, subConns := setupTest()
+}
+
+type subConn struct {
+	addrs []resolver.Address
+}
+
+func (s *subConn) Connect() {}
+
+func (s *subConn) UpdateAddresses(addrs []resolver.Address) {
+	s.addrs = addrs
+}
+
+func setupTest() (*loadbalance.Picker, []*subConn) {
+	var subConns []*subConn
+	buildInfo := base.PickerBuildInfo{
+		ReadySCs: make(map[balancer.SubConn]base.SubConnInfo),
+	}
+	for i := 0; i < 3; i++ {
+		sc := &subConn{}
+		addr := resolver.Address{
+			Attributes: attributes.New("is_leader", i == 0),
+		}
+		// 0th sub conn is the leader
+		sc.UpdateAddresses([]resolver.Address{addr})
+		buildInfo.ReadySCs[sc] = base.SubConnInfo{Address: addr}
+		subConns = append(subConns, sc)
+	}
+	picker := &loadbalance.Picker{}
+	picker.Build(buildInfo)
+	return picker, subConns
 }
