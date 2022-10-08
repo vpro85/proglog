c93055da8d16fd3b4f096ee61158b54616105abb
Vadim Prokoshin <vpro85@gmail.com>
2022-10-08 10:53:26 +0200

    remove some files

--- cmd/getservers/main.go
+++ cmd/getservers/main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	api "proglog/api/v1"
 )
@@ -13,8 +12,8 @@ import (
 func main() {
 	addr := flag.String("addr", ":8400", "service address")
 	flag.Parse()
-	//conn, err:= grpc.Dial(*addr, grpc.WithInsecure())
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	//conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal(err)
 	}
